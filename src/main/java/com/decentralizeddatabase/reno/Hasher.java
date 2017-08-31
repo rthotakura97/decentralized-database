82b3ce3a7518959e7e68278af92be010767fc0dd
Rohit Thotakaura <rthotakura97@gmail.com>
2017-08-31 16:50:22 -0500

    working on hashing

---
+++ src/main/java/com/decentralizeddatabase/reno/Hasher.java
@@ -0,0 +1,11 @@
+package com.decentralizeddatabase.reno;
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hashing;
+
+public class Hasher {
+
+    public String createBlockKey(String secretKey, String filename, int blockNumber){
+        return "Hi";
+    }
+
+}
