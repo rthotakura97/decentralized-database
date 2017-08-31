4817ab12a6ec8cef33d048571310ccdf0e535401
Rohit Thotakaura <rthotakura97@gmail.com>
2017-08-31 14:46:25 -0500

    update jetty test

--- src/main/java/com/decentralizeddatabase/DecentralizedDDB.java
+++ src/main/java/com/decentralizeddatabase/DecentralizedDDB.java
@@ -8,23 +8,31 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import static sun.net.www.protocol.http.AuthCacheValue.Type.Server;
+
 public class DecentralizedDDB extends AbstractHandler {
 
+	public String val;
     private static final int PORT = 8080;
 
+    DecentralizedDDB(String val){
+		this.val = val;
+	}
+
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-	throws IOException, ServletException {
-	response.setContentType("text/html;charset=utf-8");
-	response.setStatus(HttpServletResponse.SC_OK);
-	baseRequest.setHandled(true);
-	response.getWriter().println("<h1>Hello Tatez<h1>");
+	    throws IOException, ServletException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println(val);
     }
 
     public static void main(String[] args) throws Exception {
-	final Server server = new Server(PORT);
-	server.setHandler(new DecentralizedDDB());
-
-	server.start();
-	server.join();
+        final Server server = new Server(PORT);
+        DecentralizedDDB ddb = new DecentralizedDDB("Hello");
+        server.setHandler(ddb);
+    
+        server.start();
+        server.join();
     }
 }
