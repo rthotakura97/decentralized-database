09942c90467e0803c9af049486eb220f50248b32
Rohit Thotakaura <rthotakura97@gmail.com>
2017-09-01 18:04:16 -0500

    createBlockKey hash function added, hasher class initialized

--- src/main/java/com/decentralizeddatabase/reno/Hasher.java
+++ src/main/java/com/decentralizeddatabase/reno/Hasher.java
@@ -5,6 +5,10 @@ import com.google.common.hash.Hashing;
 
 public class Hasher {
 
+    /*
+      Input: String (secretkey), String (filename), Int (blocknumber)
+      Output: String (20byte SHA-1 hash)
+     */
     public String createBlockKey(String secretKey, String filename, int blockNumber){
          String hashed = Hashing.sha1()
                  .hashString(secretKey+filename+Integer.toString(blockNumber), Charsets.UTF_8)
