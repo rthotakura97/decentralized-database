dee41f015c731c89ed583a75efccc8ebf9c53b70
Tim <tdh911@gmail.com>
2017-09-04 22:10:14 -0500

    Cleaned up code

--- src/main/java/com/decentralizeddatabase/reno/DataManipulator.java
+++ src/main/java/com/decentralizeddatabase/reno/DataManipulator.java
@@ -5,19 +5,25 @@ import java.util.List;
 
 public class DataManipulator {
 
+    private static final double NUM_BLOCKS = 4.0;
+
     /**
      *
      * @param file File represented as string
      * @return List of byte arrays representing the broken file
      */
-    public List<byte[]> breakFile(String file){
-        List<byte[]> fileSegments = new ArrayList<byte[]>();
-        int sizeOfSegment = (int) Math.ceil(file.length()/4.0);
+    public List<byte[]> breakFile(final String file){
+	final List<byte[]> fileSegments = new ArrayList<>();
+
+	final int length = file.length();
+        final int sizeOfSegment = (int) Math.ceil(length / NUM_BLOCKS);
+
         int index = 0;
-        while(index < file.length()){
-            fileSegments.add((file.substring(index, Math.min(index+sizeOfSegment, file.length()))).getBytes());
-            index+=sizeOfSegment;
+        while (index < length) {
+            fileSegments.add((file.substring(index, Math.min(index+sizeOfSegment, length))).getBytes());
+            index += sizeOfSegment;
         }
+
         return fileSegments;
     }
 
