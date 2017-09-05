7a046444eada08b17dd88a5e72bca1ca5b1705a0
Rohit Thotakaura <rthotakura97@gmail.com>
2017-09-05 00:31:11 -0500

    skeleton for reno delete finished

--- src/main/java/com/decentralizeddatabase/reno/Reno.java
+++ src/main/java/com/decentralizeddatabase/reno/Reno.java
@@ -4,6 +4,9 @@ import com.decentralizeddatabase.errors.BadRequest;
 import com.decentralizeddatabase.utils.DecentralizedDBRequest;
 import com.decentralizeddatabase.utils.DecentralizedDBResponse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.decentralizeddatabase.utils.Constants.*;
 
 //TODO
@@ -26,6 +29,14 @@ public class Reno {
 
     public void delete(final DecentralizedDBRequest request,
 		       final DecentralizedDBResponse response) throws BadRequest {
+
+        List<String> blockKeys = new ArrayList<String>();
+        Hasher hashFunction = new Hasher();
+
+        for(int i=0; i<4; i++)
+            blockKeys.add(hashFunction.createBlockKey(request.getSecretKey(), request.getFilename(), i));
+
+        //TODO: List of keys for blocks created, need to send to jailcell to retrieve blocks
 
     }
 }
