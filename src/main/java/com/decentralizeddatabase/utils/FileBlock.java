84b7e4bf8e964b8409bea9b81260ae459b41d5d8
Tim <tdh911@gmail.com>
2017-12-02 15:35:34 -0600

    Implemented encoding order into data string

--- src/main/java/com/decentralizeddatabase/utils/FileBlock.java
+++ src/main/java/com/decentralizeddatabase/utils/FileBlock.java
@@ -1,5 +1,7 @@
 package com.decentralizeddatabase.utils;
 
+import java.math.BigInteger;
+
 public class FileBlock {
 
     private final long blockOrder;
@@ -10,11 +12,38 @@ public class FileBlock {
         this.encryptedData = encryptedData;
     }
 
+    /**
+     * @param encodedData String returned by JailCell
+     * 
+     * Builds a FileBlock from raw data returned by JailCell
+     */
+    public FileBlock(final String encodedData) {
+        final String order = encodedData.substring(0,16);
+        blockOrder = new BigInteger(order, 16).longValue();
+        encryptedData = encodedData.substring(16);
+    }
+
     public long getBlockOrder() {
         return blockOrder;
     }
 
     public String getData() {
         return encryptedData;
+    }
+
+    /**
+     * @return  A blob with the first 64 bits representing a signed long, the rest of the block represents
+     *          the encrypted string
+     * 
+     * Constructs and returns a string with encoded blockOrder into encryptedData
+     */
+    public String encodeOrderIntoBlock() {
+        final StringBuilder order = new StringBuilder(Long.toHexString(blockOrder));
+        while (order.length() < 16) {
+            order.insert(0, "0");
+        }
+        order.append(encryptedData);
+
+        return order.toString();
     }
 }
---
+++ src/test/java/com/decentralizeddatabase/utils/FileBlockTest.java
@@ -0,0 +1,86 @@
+package com.decentralizeddatabase.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FileBlockTest {
+    private static final long BLOCKORDER = 982834919L;
+    private static final String DATA = "laafelisjealf2839238921j3jrl";
+
+    private static String encoderHelper(final long order, final String data) {
+        final StringBuilder orderBuilder = new StringBuilder(Long.toHexString(order));
+        while (orderBuilder.length() < 16) {
+            orderBuilder.insert(0, "0");
+        }
+        orderBuilder.append(data);
+
+        return orderBuilder.toString();
+    }
+
+    @Test
+    public void testBuildFileBlockFromEncodedData() {
+        final FileBlock expected = new FileBlock(BLOCKORDER, DATA);
+        final String encodedData = encoderHelper(BLOCKORDER, DATA);
+        
+        final FileBlock actual = new FileBlock(encodedData);
+
+        Assert.assertEquals(expected.getBlockOrder(), actual.getBlockOrder());
+        Assert.assertEquals(expected.getData(), actual.getData());
+    }
+
+    @Test
+    public void testBuildFileBLockFromEncodedDataWithNegativeLong() {
+        final FileBlock expected = new FileBlock(-123456789L, DATA);
+        final String encodedData = encoderHelper(-123456789L, DATA);
+        
+        final FileBlock actual = new FileBlock(encodedData);
+
+        Assert.assertEquals(expected.getBlockOrder(), actual.getBlockOrder());
+        Assert.assertEquals(expected.getData(), actual.getData());
+    }
+
+    @Test
+    public void testBuildFileBlockFromEncodedDataWithMaxLong() {
+        final FileBlock expected = new FileBlock(Long.MAX_VALUE, DATA);
+        final String encodedData = encoderHelper(Long.MAX_VALUE, DATA);
+        
+        final FileBlock actual = new FileBlock(encodedData);
+
+        Assert.assertEquals(expected.getBlockOrder(), actual.getBlockOrder());
+        Assert.assertEquals(expected.getData(), actual.getData());
+
+    }
+
+    @Test
+    public void testBuildFileBlockFromEncodedDataWithMinLong() {
+        final FileBlock expected = new FileBlock(Long.MIN_VALUE, DATA);
+        final String encodedData = encoderHelper(Long.MIN_VALUE, DATA);
+        
+        final FileBlock actual = new FileBlock(encodedData);
+
+        Assert.assertEquals(expected.getBlockOrder(), actual.getBlockOrder());
+        Assert.assertEquals(expected.getData(), actual.getData());
+
+    }
+
+    @Test
+    public void testBuildFileBlockFromEncodedDataWithZero() {
+        final FileBlock expected = new FileBlock(0, DATA);
+        final String encodedData = encoderHelper(0, DATA);
+        
+        final FileBlock actual = new FileBlock(encodedData);
+
+        Assert.assertEquals(expected.getBlockOrder(), actual.getBlockOrder());
+        Assert.assertEquals(expected.getData(), actual.getData());
+    }
+
+    @Test
+    public void testEncodeFileBlockIntoData() {
+        final FileBlock fileBlock = new FileBlock(BLOCKORDER, DATA);
+        final String expected = encoderHelper(BLOCKORDER, DATA);
+
+        final String actual = fileBlock.encodeOrderIntoBlock();
+
+        Assert.assertEquals(expected, actual);
+    }
+}
